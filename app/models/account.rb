01c9bf76d8ff39a4282276adde5efd9f73958c2b
JoeButikofer <joebutikofer@gmail.com>
2015-03-12 12:39:59 +0000

    Adding the destroy dependances and the uniq constraints

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,4 @@
 class Account < ActiveRecord::Base
-  has_and_belongs_to_many :trackgroups
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :trackgroups, -> { uniq }
+  has_and_belongs_to_many :users, -> { uniq }
 end
