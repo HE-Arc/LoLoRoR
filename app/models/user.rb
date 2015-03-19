5ff29f768e8716ed8a8e80c941541ff67ef9b303
JoeButikofer <joebutikofer@gmail.com>
2015-03-19 12:33:20 +0000

    Login with username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,27 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+  attr_accessor :login
+  
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_h).where(["username = :value OR email = :value", { :value => login.downcase }]).first
+      else
+        where(conditions.to_h).first
+      end
+    end
+  
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  } 
+  
   has_many :trackgroups, dependent: :destroy
   has_many :dashboards, dependent: :destroy
   has_and_belongs_to_many :accounts, -> { uniq }
