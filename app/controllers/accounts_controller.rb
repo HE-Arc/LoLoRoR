2357c059b1fb338fb5ddae02fb4e1e8ccb67eafc
JoeButikofer <joebutikofer@gmail.com>
2015-03-26 16:18:26 +0000

    Bugs with account path fixed and search function done

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,23 +4,12 @@ class AccountsController < ApplicationController
   before_action :authenticate_user!, :except => [:show]
   
   def show
-    #Find the summoner with the corresponding id and region
-    @summoner = LOL_WRAPPER.get_summoner_by_id(params[:idLoL],params[:region])
     
-    #Find the stats of the summoner with the corresponding id and region
-    @stats = LOL_WRAPPER.get_account_infos(params[:idLoL],params[:region])
+    @idLoL = params[:idLoL]
+    @region = params[:region]
     
-    #TODO check error
+    find_account_and_save
     
-    #Create or update the corresponding account in our database
-    if Account.exists?(:idLoL => params[:idLoL])
-      @account = Account.find_by_idLoL(params[:idLoL])
-      @account.pseudoLoL = @summoner.name
-      @account.save
-    else
-      @account = Account.new(idLoL: params[:idLoL], region: params[:region], pseudoLoL: @summoner.name)
-      @account.save
-    end
     if user_signed_in?
       @user = current_user
       @trackgroups = @user.trackgroups
@@ -33,9 +22,34 @@ class AccountsController < ApplicationController
   end
   
   def searchAccounts
-    #TODO utiliser l'API
-    @accounts = Account.all
-    render "showUserAccounts"
+    
+    @idLoL = LOL_WRAPPER.get_summoner_id(params[:name],  params[:region])
+    @region = params[:region]
+    
+    find_account_and_save
+    
+    render "show"
+  end
+  
+  private
+  def find_account_and_save
+   #Find the summoner with the corresponding id and region
+    @summoner = LOL_WRAPPER.get_summoner_by_id(@idLoL,@region)
+    
+    #Find the stats of the summoner with the corresponding id and region
+    @stats = LOL_WRAPPER.get_account_infos(@idLoL,@region)
+    
+    #TODO check error
+    
+    #Create or update the corresponding account in our database
+    if Account.exists?(:idLoL => @idLoL)
+      @account = Account.find_by_idLoL(@idLoL)
+      @account.pseudoLoL = @summoner.name
+      @account.save
+    else
+      @account = Account.new(idLoL: @idLoL, region: @region, pseudoLoL: @summoner.name)
+      @account.save
+    end
   end
   
 end
