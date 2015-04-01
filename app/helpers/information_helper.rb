db71a384572da000b2f4e495da850ff45682f159
JoeButikofer <joebutikofer@gmail.com>
2015-04-01 08:00:54 +0000

    Fixing a weird error

--- app/helpers/information_helper.rb
+++ app/helpers/information_helper.rb
@@ -1,2 +1,2 @@
-module InformationsHelper
+module InformationHelper
 end
--- test/controllers/information_controller_test.rb
+++ test/controllers/information_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class InformationsControllerTest < ActionController::TestCase
+class InformationControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
