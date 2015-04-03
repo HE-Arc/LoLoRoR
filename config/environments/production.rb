b2059b52853bd8e8ef059bb803914e2829670be3
JoeButikofer <joebutikofer@gmail.com>
2015-04-03 20:38:38 +0000

    Added confirmation for user registration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default_url_options = { :host => 'loloror.ch' }
+  config.action_mailer.default_url_options = { :host => 'loltracker.rails.teleinf.labinfo.eiaj.ch' }
 
   ActionMailer::Base.smtp_settings = {  
     :address              => "smtp.gmail.com",  
