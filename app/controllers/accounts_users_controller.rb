b09daa9580fcb46bc46860f8d3315031af07bc4b
JoeButikofer <joebutikofer@gmail.com>
2015-04-04 19:41:42 +0000

    remote debug

--- app/controllers/accounts_users_controller.rb
+++ app/controllers/accounts_users_controller.rb
@@ -5,11 +5,14 @@ class AccountsUsersController < ApplicationController
   
   # Create an association between the current user and the account
   def create
-     
+    
     @user = current_user
+    puts "0"
     @account = Account.find(params[:account][:id])
     @user.accounts << @account
+    puts "1"
     redirect_to account_path(:region => @account.region, :idLoL => @account.idLoL)
+    puts "2"
     
   end
   
