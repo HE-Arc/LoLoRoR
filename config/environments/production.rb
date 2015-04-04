93eb7a3a16a2b0a42812fb64b1f7a434e5ae52df
JoeButikofer <joebutikofer@gmail.com>
2015-04-04 20:10:08 +0000

    remote debug 5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.assets.raise_runtime_errors = true
   
   config.action_controller.relative_url_root = '/loloror'
   config.action_mailer.raise_delivery_errors = false
