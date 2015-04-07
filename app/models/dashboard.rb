4e13d35408f966ac0fd8b933fc41d4e90b6d0e6b
JoeButikofer <joebutikofer@gmail.com>
2015-04-07 10:31:48 +0000

    Added validation for models

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -2,4 +2,6 @@ class Dashboard < ActiveRecord::Base
   belongs_to :user
   has_many :match_history_modules, dependent: :destroy
   has_many :top_champions_modules, dependent: :destroy
+  
+  validates :name, presence: true, allow_blank: false
 end
